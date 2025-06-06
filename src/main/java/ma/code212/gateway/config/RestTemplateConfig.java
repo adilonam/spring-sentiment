28fca835ea134db687de9f0321ae3f7c99c843e9
Adil ABBADI <adil.abbadi.1996@gmail.com>
2025-06-06 15:39:52 +0100

    good for proxy to fast pai

--- src/main/java/ma/code212/gateway/config/RestTemplateConfig.java
+++ src/main/java/ma/code212/gateway/config/RestTemplateConfig.java
@@ -5,34 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
-import javax.net.ssl.*;
-import java.security.KeyManagementException;
-import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
-import java.security.cert.X509Certificate;
-
 @Configuration
 public class RestTemplateConfig {
 
     @Bean
-    public RestTemplate restTemplate() throws NoSuchAlgorithmException, KeyManagementException {
-        // Create a trust manager that accepts all certificates
-        TrustManager[] trustAllCerts = new TrustManager[] {
-            new X509TrustManager() {
-                public X509Certificate[] getAcceptedIssuers() { return null; }
-                public void checkClientTrusted(X509Certificate[] certs, String authType) { }
-                public void checkServerTrusted(X509Certificate[] certs, String authType) { }
-            }
-        };
-
-        // Create SSL context with the trust manager
-        SSLContext sslContext = SSLContext.getInstance("TLS");
-        sslContext.init(null, trustAllCerts, new SecureRandom());
-
-        // Set the default SSL context and hostname verifier
-        HttpsURLConnection.setDefaultSSLSocketFactory(sslContext.getSocketFactory());
-        HttpsURLConnection.setDefaultHostnameVerifier((hostname, session) -> true);
-
+    public RestTemplate restTemplate() {
         // Create RestTemplate with simple client factory and timeouts
         SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
         requestFactory.setConnectTimeout(10000); // 10 seconds
